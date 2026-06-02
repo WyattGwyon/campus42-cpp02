279686ec9b162e1b1eb14913cf5204e6eab431ef
Clayton Louden <claytonlouden@gmail.com>
2026-06-02 16:06:20 +0200

    small addition to main of ex01 to show raw bits of 1

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: clouden <clouden@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/16 16:05:21 by clouden           #+#    #+#             */
-/*   Updated: 2026/05/19 18:58:32 by clouden          ###   ########.fr       */
+/*   Updated: 2026/06/02 16:04:10 by clouden          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,4 +30,9 @@ int	main(void)
 	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
 	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
 	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+
+	Fixed	const e(1);
+	std::cout << "e is " << e << std::endl;
+	std::cout << "e raw is " << e.getRawBits() << std::endl;
+
 }
